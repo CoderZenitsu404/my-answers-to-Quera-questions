0c9c8f6308246eb3b03e3d1458d1f29e845ef269
Parsa Akbari <coder.zenitsu@gmail.com>
2025-02-04 23:45:36 +0330

    Sherlock’s Speedy Chase

---
+++ Sherlock’s Speedy Chase/Sherlock’s Speedy Chase Answer.py
@@ -0,0 +1,30 @@
+# Reghabat Ba Zaman
+
+from math import ceil
+
+k = int(input()) # speed (m/s)
+n = int(input()) # number of buildings 
+a = list(map(int, input().split())) # height of each building
+i = 0 # variable (number of building)
+h = 0 # his location's height
+target = 0 # target height
+total = 0 # total time
+
+while i < n: # main loop
+
+    target = a[i] # target height
+ 
+    if target > h: # if target is higher than his location
+        temp = (target - h) / k # time
+        total += ceil(temp) # add to total
+        h = target # update
+
+    else: # if target is lower than or equal to his location 
+        temp = (h - target) / k # time
+        total += ceil(temp) # add to total
+        h = target # update
+
+    i += 1 # update
+
+total += n + ceil(h/k) # add to total
+print(total)
